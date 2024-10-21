4c93bc5695c17c9fa1e9518b6d0ccece1a89ba06
Maple0922 <maple0922sept@gmail.com>
2024-10-21 10:57:12 +0900

    feat: アイコンなどをLFMSに変更

--- src/components/pages/list/index.vue
+++ src/components/pages/list/index.vue
@@ -1,52 +1,7 @@
 <template>
   <v-row justify="end">
-    <v-col>
-      <v-btn
-        color="indigo-darken-4"
-        prepend-icon="mdi-calendar-today"
-        variant="outlined"
-        @click="shiftToday"
-      >
-        Today
-      </v-btn>
-    </v-col>
-    <v-spacer />
-    <v-col align="end">
-      <v-btn
-        prepend-icon="mdi-bell-ring"
-        variant="text"
-        color="green text-none"
-        @click="onClickLineBot"
-      >
-        LINEBot
-      </v-btn>
-    </v-col>
-    <v-col v-if="false">
-      <v-btn-toggle
-        v-model="type"
-        rounded="md"
-        color="indigo-darken-4"
-        group
-        mandatory
-        border
-        density="comfortable"
-      >
-        <v-btn value="d" @click="onChangeType">Daily</v-btn>
-        <v-btn value="w" @click="onChangeType">Weekly</v-btn>
-      </v-btn-toggle>
-    </v-col>
+    <v-col cols="12" md="8" lg="6"> あ </v-col>
   </v-row>
-  <v-row>
-    <v-col v-if="type === 'd'">
-      <daily-timeline :day="day" @shift="shiftDate" />
-    </v-col>
-    <v-col v-else-if="type === 'w'">
-      <p class="text-h6 font-weight-bold" v-for="(day, key) in days" :key="key">
-        {{ formatDate(day.date, "M/D(ddd)") }}
-      </p>
-    </v-col>
-  </v-row>
-  <snackbar />
 </template>
 
 <script lang="ts" setup>
